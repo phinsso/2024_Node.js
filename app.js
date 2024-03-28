912b6a8046916e696f3a7854aef40fb543ea9e90
phinsso <naso01247@gmail.com>
2024-03-28 15:55:59 +0900

    express 기본 예제

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// get method 요청에 대한 라우팅
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
